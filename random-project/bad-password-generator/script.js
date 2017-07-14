e19bcbd7ceda0ce638837a16c7f390f335f7b832
Noah Loomans <noahloomans@gmail.com>
2017-07-14 21:02:46 +0200

    Create bad-password-generator random project

---
+++ random-project/bad-password-generator/script.js
@@ -0,0 +1,16 @@
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+window.fetch('./8char-words.txt').then(r => r.text()).then(res => {
+  const words = res.split('\n')
+  const specialChars = ['.', '-', '_', '!', '@', '#', '$', '%']
+
+  window.setInterval(function () {
+    const randomWord = words[getRandomInt(0, words.length - 1)]
+    const randomSpecialChar = specialChars[getRandomInt(0, specialChars.length - 1)]
+    const randomNumber = getRandomInt(1, 9)
+    const randomPassword = randomWord + randomSpecialChar + randomNumber
+    document.querySelector('#changing-passwd').textContent = randomPassword
+  }, 500)
+})
